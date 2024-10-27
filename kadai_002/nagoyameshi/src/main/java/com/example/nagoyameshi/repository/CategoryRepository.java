844e8cf70f93af80a8072da0b5e0c161f1cb736c
MaiT <55681728+MAI0521@users.noreply.github.com>
2024-10-27 13:19:23 +0900

    カテゴリー名での検索

--- kadai_002/nagoyameshi/src/main/java/com/example/nagoyameshi/repository/CategoryRepository.java
+++ kadai_002/nagoyameshi/src/main/java/com/example/nagoyameshi/repository/CategoryRepository.java
@@ -3,10 +3,14 @@ package com.example.nagoyameshi.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.nagoyameshi.entity.Category;
 
  public interface CategoryRepository extends JpaRepository<Category, Integer> {
 	 List<Category> findAll();
+	 @Query("SELECT c FROM Category c WHERE c.name LIKE %:keyword%")
+	 List<Category> findByKeyword(@Param("keyword") String keyword);
 	 public Category getCategoryById(Integer id);
  }
